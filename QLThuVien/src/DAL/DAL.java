9a2c808ab97c16a7f95fa25e6cb7c56c84afbf84
Phuoc Hoang Truong <langtucodoc07031998@gmail.com>
2019-05-01 19:41:23 +0700

    bat dau tu dau

---
+++ QLThuVien/src/DAL/DAL.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAL;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author langt
+ */
+public class DAL {
+    public Connection  getConnection() {
+        Connection conn = null;
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            conn = DriverManager.getConnection("jdbc:sqlserver://localhost:1433;databasename=QuanLyThuVien;");
+        } catch (ClassNotFoundException | SQLException ex) {
+            Logger.getLogger(DAL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return conn;
+    }
+    
+    public ResultSet getTable(String sql) {
+        ResultSet rs = null;
+        try {
+            Connection conn = getConnection();
+            PreparedStatement ps = conn.prepareCall(sql);
+            rs = ps.executeQuery();
+        } catch (SQLException ex) {
+            Logger.getLogger(DAL.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return rs;
+    }
+}
